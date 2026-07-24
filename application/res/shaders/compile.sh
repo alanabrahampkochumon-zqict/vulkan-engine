a09fda9d908d03a655236abcc229648d55427088
Alan Abraham Puthenparambil Kochumon <52540085+alanabrahampkochumon-zqict@users.noreply.github.com>
2026-07-24 16:17:33 +0530

    feat: add basic shader compilation script

---
+++ application/res/shaders/compile.sh
@@ -0,0 +1,3 @@
+echo "Update the script wit your VulkanSDK Path/Version!"
+/home/user/VulkanSDK/1.4.350.0/x86_64/bin/glslc shader.vert -o vert.spv
+/home/user/VulkanSDK/1.4.350.0/x86_64/bin/glslc shader.frag -o frag.spv
